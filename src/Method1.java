aac7b8929c0075513541125bfb978e3a6a3f9e99
William Breen <wbreen@carpenter-11.elon.edu>
2017-03-12 18:33:11 -0400

    trying again, worked on method 1

--- src/Method1.java
+++ src/Method1.java
@@ -3,8 +3,8 @@
  * CSC 331 Programming Assignment 2
  * Method 1
  *
- * @author Scott Spurlock
- * @version 2/28/17
+ * @author Scott Spurlock and William Breen
+ * @version 3/12/17
  */
 public class Method1 {
 
@@ -22,9 +22,29 @@ public class Method1 {
 	 * @return an array of size k holding the largest k elements of A
 	 */
 	public int[] findTopK(int[] arr, int k) {
+		//create all the required variables
+		int arrLength = arr.length;
 		int[] results = new int[k];
-
-		// TBD
+		//initializes all as false, so if it is chosen, change the same one to true
+		boolean[] hasBeenChosen = new boolean[arrLength];
+		
+		//loop through the array k times, and put the max value found each time into the results array
+		for (int i = 0; i<k; i++){
+			//"largest" = the largest value, "largestLoc" = location of this value in array
+			int largest = arr[0];
+			int largestLoc = 0;
+			
+			for (int j = 0; j<arrLength; j++){
+				if (arr[j] >= largest && !hasBeenChosen[j]){
+					largest = arr[j];
+					largestLoc = j;
+				}
+				
+			}
+			results[i] = largest;
+			hasBeenChosen[largestLoc] = true;
+		}
+		
 		
 		return results;
 	}
